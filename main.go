65bf3ece10c0ff17cf4688ef027f4b27fe848b48
quzhongling <quzhongling@dm-ai.cn>
2020-11-18 17:55:33 +0800

    添加接口

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+	hostname, _ := os.Hostname()
+	addrs, _ := net.InterfaceAddrs()
+	var ip string
+	for _, address := range addrs {
+		// 检查ip地址格式
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ip = ip + ipnet.IP.String()
+			}
+
+		}
+	}
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello, 我是默认页")
 	})
+
+	r.GET("/hostname", func(c *gin.Context) {
+		c.String(200, hostname)
+	})
+
+	r.GET("/ip", func(c *gin.Context) {
+		c.String(200, ip)
+	})
+
 	r.Run(":" + "80") // listen and serve on 0.0.0.0:80
 }
