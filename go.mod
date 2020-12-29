61f4dd9125c9db4a1f4a29d559e87b2be6bde8c3
quzhongling <zlingqu@126.com>
2020-12-29 11:39:04 +0800

    添加接口

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	gorm.io/driver/mysql v1.0.3 // indirect
 	gorm.io/gorm v1.20.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"net"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	qrcode "github.com/skip2/go-qrcode"
 )
+
+type ReqQrcode struct {
+	Url string `form:"url"`
+}
+
+func GetQrcode(c *gin.Context) {
+	var reqQrcode ReqQrcode
+	// var code int
+	var png []byte
+	c.ShouldBind(&reqQrcode)
+
+	png, _ = qrcode.Encode(reqQrcode.Url, qrcode.Medium, 256)
+	// png, _ = json.Marshal(png)
+	c.String(http.StatusOK, string(png))
+	// c.Data(http.StatusOK, "bytes", png)
+}
 
 func main() {
 
@@ -34,6 +52,8 @@ func main() {
 	r.GET("/ip", func(c *gin.Context) {
 		c.String(200, ip)
 	})
+
+	r.GET("/qrcode", GetQrcode)
 
 	r.Run(":" + "80") // listen and serve on 0.0.0.0:80
 }
